9f13931ed8524151c12ee0efe67ba8ebff2ee92b
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-10-19 23:49:22 +0300

    add bootstrap

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <button className="btn btn-danger">Landing Page</button>
+      <button className="btn btn-success">Landing Page</button>
+    </div>
+  );
+}
