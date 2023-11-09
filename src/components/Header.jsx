7331ec67510c79027707a31772207731e84b3160
Alex Jarabi <jarabialex@gmail.com>
2023-11-09 07:38:37 +0300

    Added navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,19 +6,55 @@ export default function Header({ userData, isLoggedIn, loginState }) {
       document.body.scrollTop > 50 ||
       document.documentElement.scrollTop > 50
     ) {
-      document.querySelector("header").style.padding = "0.25rem";
+      document.querySelector("header").style.padding = "0.5rem";
+      document.querySelector("header").style.fontSize = "0.9rem";
+      document.querySelector(".header-title").style.fontSize = "1.5rem";
+      document.querySelector(".header-img").style.width = "35px";
     } else {
       document.querySelector("header").style.padding = "0.5rem";
+      document.querySelector("header").style.fontSize = "1rem";
+      document.querySelector(".header-title").style.fontSize = "2rem";
+      document.querySelector(".header-img").style.width = "57px";
     }
   };
 
   return (
     <header>
-      <div className='container'>
+      <div className='container d-flex justify-content-between'>
         <div className='header-logo d-flex flex-row align-items-center'>
-          <img src={headerLogo} alt='Header Logo' />
-          <h2 className='ms-2'>inventorypro</h2>
+          <a href='/'>
+            <img className='header-img' src={headerLogo} alt='Header Logo' />
+          </a>
+          <h2 className='header-title ms-2'>inventorypro</h2>
         </div>
+
+        <nav className='navbar navbar-expand-lg bg-body-light'>
+          <div className='container-fluid'>
+            <div className='collapse navbar-collapse' id='navbarNav'>
+              <ul className='navbar-nav px-5'>
+                <li className='nav-item'>
+                  <a
+                    className='nav-link active'
+                    aria-current='page'
+                    href='#hero'
+                  >
+                    Home
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link' href='#features'>
+                    Features
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link' href='#about'>
+                    About
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
 
         {/* {isLoggedIn && (
           <>
