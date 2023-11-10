d540c10a37d63d5008b8885736c4022d3f368e00
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-11-10 13:05:37 +0300

    create auth api

---
+++ src/api/auth.js
@@ -0,0 +1,24 @@
+import client from "./client";
+import { AUTH_TOKEN } from "./constants";
+
+/**
+ *
+ * @param { {email: string, password: string} } credentials
+ */
+const loginUser = async (credentials) => {
+  try {
+    const response = await client.post("/auth/login", credentials);
+    if (response.status === 201) {
+      localStorage.setItem(AUTH_TOKEN, response.data.accessToken);
+    }
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+const authApi = {
+  loginUser,
+};
+
+export default authApi;
