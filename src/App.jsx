1d51697fcbfa9e2651d9701d90d5cbedb03ff9d0
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-11-11 12:43:37 +0300

    clean up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "../src/styles/App.css";
 import Header from "./components/Header";
@@ -7,37 +6,19 @@ import Login from "./components/Login";
 import UserView from "./components/UserView";
 import Footer from "./components/Footer";
 import RequisitionView from "./components/RequisitionView";
-import { AUTH_TOKEN } from "./api/constants";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [users, setUsers] = useState([]);
-  const [requisitions, setRequisitions] = useState([]);
-  const [userData, setUserData] = useState([]);
-
-  const checkLoggedInStatus = async () => {
-    const token = await localStorage.getItem(AUTH_TOKEN);
-    if (!token) {
-      setIsLoggedIn(false);
-    } else {
-      setIsLoggedIn(true);
-    }
-  };
-  useEffect(() => {
-    checkLoggedInStatus();
-  }, []);
-
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<>{!isLoggedIn && <LandingPage />}</>} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/user"
-          element={<UserView userData={userData} requisitions={requisitions} />}
-        />
-        <Route path="/requisitionView" element={<RequisitionView />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/user" element={<UserView />} />
+          <Route path="/requisitionView" element={<RequisitionView />} />
+        </Route>
       </Routes>
       <Footer />
     </>
--- src/components/UserView.jsx
+++ src/components/UserView.jsx
@@ -1,46 +1,44 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import Requisitions from "./Requisitions";
 // import NewRequest from "../components/NewRequest";
 
-const UserView = ({ userData, requisitions }) => {
-  const [showNewRequest, setShowNewRequest] = useState(false);
-
+const UserView = () => {
+  const requisitions = [];
   return (
-    <div className='container user-page'>
-      <div className='top-section d-flex justify-content-end mb-3'>
+    <div className="container user-page">
+      <div className="top-section d-flex justify-content-end mb-3">
         <Link
-          className='btn btn-outline-primary'
-          type='button'
-          id='new-request'
-          to='/new-requisition'
+          className="btn btn-outline-primary"
+          type="button"
+          id="new-request"
+          to="/new-requisition"
         >
-          <i className='bi bi-plus-square-dotted'></i> New Requisition
+          <i className="bi bi-plus-square-dotted"></i> New Requisition
         </Link>
-        <div className='search d-flex align-items-end ms-3'>
-          <div className='input-group search-box'>
+        <div className="search d-flex align-items-end ms-3">
+          <div className="input-group search-box">
             <input
-              type='text'
-              className='form-control search-requisition'
-              placeholder='Search'
-              aria-label='search'
-              aria-describedby='search'
+              type="text"
+              className="form-control search-requisition"
+              placeholder="Search"
+              aria-label="search"
+              aria-describedby="search"
             />
             <button
-              className='btn btn-outline-secondary'
-              type='button'
-              id='search'
+              className="btn btn-outline-secondary"
+              type="button"
+              id="search"
             >
-              <i className='bi bi-search'></i>
+              <i className="bi bi-search"></i>
             </button>
           </div>
         </div>
       </div>
-      <h5 className='requisitions-view text-center'>User Requisitions</h5>
+      <h5 className="requisitions-view text-center">User Requisitions</h5>
       {requisitions.length > 0 ? (
-        <Requisitions userData={userData} requisitions={requisitions} />
+        <Requisitions requisitions={requisitions} />
       ) : (
-        <div className='alert alert-info text-center' role='alert'>
+        <div className="alert alert-info text-center" role="alert">
           No Requisitions Yet.
         </div>
       )}
