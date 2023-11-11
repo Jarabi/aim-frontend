918725ba9204b12f6582a854daa1a63323dc6aef
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-11-11 16:41:23 +0300

    update dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,14 @@
+import UserView from "./UserView";
+
 export default function Dashboard() {
+  //fetch user, user role to render the right dashboard
+  const role = "regular"; // obtain from store i.e user.role
+
+  if (role === "regular") return <UserView />;
+
+  //admin
+  //manager
+
   return (
     <div className="container" style={{ height: "500px", marginTop: "100px" }}>
       Hello Dashboard
