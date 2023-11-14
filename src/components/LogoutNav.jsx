ad4335664f2d72c4caf815d61ca6645b8500506a
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-11-14 10:18:04 +0300

    ad navigation to dashboard on dropdown menu

--- src/components/LogoutNav.jsx
+++ src/components/LogoutNav.jsx
@@ -1,10 +1,10 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import authApi from "../api/auth";
 import { FaUser } from "react-icons/fa";
 import { useEffect, useState } from "react";
 import { CURRENT_USER } from "../api/constants";
 
-const LogoutNav = ({checkLoggedInStatus}) => {
+const LogoutNav = ({ checkLoggedInStatus }) => {
   const [userInfo, setUserInfo] = useState({});
   const navigate = useNavigate();
 
@@ -20,25 +20,30 @@ const LogoutNav = ({checkLoggedInStatus}) => {
   }, []);
 
   return (
-    <nav className='navbar navbar-expand-lg bg-body-light'>
-      <div className='container-fluid'>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-            <li className='nav-item dropdown'>
+    <nav className="navbar navbar-expand-lg bg-body-light">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item dropdown">
               <a
-                className='nav-link dropdown-toggle'
-                href='#'
-                role='button'
-                data-bs-toggle='dropdown'
-                aria-expanded='false'
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
               >
-                <span className='badge text-bg-primary'>{userInfo.email}</span>
+                <span className="badge text-bg-primary">{userInfo.email}</span>
 
-                <FaUser className='ms-3' />
+                <FaUser className="ms-3" />
               </a>
-              <ul className='dropdown-menu dropdown-menu-end'>
+              <ul className="dropdown-menu dropdown-menu-end">
                 <li>
-                  <div className='btn dropdown-item' onClick={logoutHandler}>
+                  <Link className="btn dropdown-item" to={"/dashboard"}>
+                    Dashboard
+                  </Link>
+                </li>
+                <li>
+                  <div className="btn dropdown-item" onClick={logoutHandler}>
                     Logout
                   </div>
                 </li>
